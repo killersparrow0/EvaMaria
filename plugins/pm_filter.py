3a9815ebb23b8b0ddac06fa0f443e830b861fee1
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-11-07 13:43:22 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -622,15 +622,7 @@ async def auto_filter(client, msg, spoll=False):
         )
     imdb = await get_poster(search) if IMDB else None
     if imdb:
-        cap = f"🎬𝙍𝙚𝙦𝙪𝙚𝙨𝙩𝙚𝙙 𝙈𝙤𝙫𝙞𝙚 : {search}\n\nIMDb Data:\n🏷Title</b>:<a href={url}>{title}</a>
-🎭 Genres: {genres}
-📆 Year: <a href={url}/releaseinfo>{year}</a>
-🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)
-☀️ Languages : <code>{languages}</code>
-👥 Cast : <code>{cast}</code>
-📀 RunTime: {runtime} Minutes
-📆 Release Info : {release_date}
-🎛 Countries : <code>{countries}</code>
+        cap = f"🎬𝙍𝙚𝙦𝙪𝙚𝙨𝙩𝙚𝙙 𝙈𝙤𝙫𝙞𝙚 : {search}\n\nIMDb Data:\n🏷Title</b>:<a href={url}>{title}</a>🎭 Genres: {genres}📆 Year: <a href={url}/releaseinfo>{year}</a>🌟 Rating: <a href={url}/ratings>{rating}</a> / 10 (based on {votes} user ratings.)☀️ Languages : <code>{languages}</code>👥 Cast : <code>{cast}</code>📀 RunTime: {runtime} Minutes📆 Release Info : {release_date}🎛 Countries : <code>{countries}</code>
 
 
         cap = "Here is what i found for your query {search}"
