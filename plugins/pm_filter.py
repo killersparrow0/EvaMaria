e3c64169665376467a4f33eed4701357a5258227
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-11-08 11:43:36 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -73,7 +73,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("Eth ningaluku ollathualla..!!\nRequest ur own files, then click here🙂", show_alert=True)
     try:
         offset = int(offset)
     except:
