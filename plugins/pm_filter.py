3df554799a8ce83d3519b5eb16aa49029135d4bc
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-11-09 17:16:54 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -670,7 +670,7 @@ async def advantage_spell_chok(msg):
                 )
             ] for k, movie in enumerate(movielist)]
     btn.append([InlineKeyboardButton(text="Close", callback_data='close_data')])
-    m = await
+    msg = await
     msg.reply("𝙉𝙤𝙩 𝙖𝙫𝙖𝙞𝙡𝙖𝙗𝙡𝙚 𝙍𝙞𝙜𝙝𝙩 𝙉𝙤𝙬🥲\n𝙋𝙤𝙨𝙨𝙞𝙗𝙡𝙚 𝘾𝙖𝙪𝙨𝙚𝙨 : 🤔\n\n⭕️ Not released yet\n⭕️ Search in Google for correct spelling\n\n[Click Here to Search in Google](www.google.com)\n\n⭕️Dont use Symbols\n⭕ Asking theatre prints\n⭕ Not in Database")
-    await asyncio.sleep(15)
+    await asyncio.sleep(10)
     await m.delete()
