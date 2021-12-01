4c971fef15e77efb3107a247f8ed42cfb501a903
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-12-01 17:19:51 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -68,7 +68,7 @@ def a(client, message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f'🎹 <b>Title:</b> <a href="{link}">{title}</a>\n🎙️ <b>Duration:</b> <code>{duration}</code>\n🎵 <b>Views:</b> <code>{views}</code>\n🎸 <b>Requested By:</b> {message.from_user.mention()} \n🎶 <b>Uploaded by: @Movies_club_2022 </b> 👑'
+        rep = f'🎹 <b>Title:</b> <a href="{link}">{title}</a>\n🎙️ <b>Duration:</b> <code>{duration}</code>\n🎵 <b>Views:</b> <code>{views}</code>'
         secmul, dur, dur_arr = 1, 0, duration.split(':')
         for i in range(len(dur_arr)-1, -1, -1):
             dur += (int(dur_arr[i]) * secmul)
