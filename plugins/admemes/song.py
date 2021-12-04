87aa8cdf529926c06be58d9294facb7f9534aca1
Ronaldabr <93811453+Ronaldabr@users.noreply.github.com>
2021-12-04 11:47:11 +0530

    Update song.py

--- plugins/admemes/song.py
+++ plugins/admemes/song.py
@@ -47,7 +47,7 @@ def a(client, message):
             #     m.edit("Exceeded 30mins cap")
             #     return
 
-            performer = f"Unknown Artist " 
+            performer = f"Unknown Artist" 
             thumb_name = f'thumb{message.message_id}.jpg'
             thumb = requests.get(thumbnail, allow_redirects=True)
             open(thumb_name, 'wb').write(thumb.content)
